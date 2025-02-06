f629ac8aeef5ddcc144dc8b63cc63522616ef329
Madhuchandrika01 <madhuchandrika041@gmail.com>
2025-02-06 01:17:04 -0600

    Updated

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const menuToggle = document.querySelector("#mobile-menu");
+    const menuToggle = document.querySelector(".menu-toggle");
     const navLinks = document.querySelector(".nav-links");
 
-    if (menuToggle && navLinks) {
-        menuToggle.addEventListener("click", () => {
-            navLinks.classList.toggle("active");
-        });
-    }
+    menuToggle.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+    });
 });
