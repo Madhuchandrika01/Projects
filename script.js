1a8e005d7a979a96be3c62a40708154ed1e27f37
Madhuchandrika01 <madhuchandrika041@gmail.com>
2025-02-06 00:47:09 -0600

    Updated

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const menuToggle = document.querySelector(".menu-toggle");
+    const menuToggle = document.querySelector("#mobile-menu");
     const navLinks = document.querySelector(".nav-links");
 
     menuToggle.addEventListener("click", () => {
